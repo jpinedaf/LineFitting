e2efe2b258a69237d23505e6b576d81204729551
Mike Chen <mkid.chen@gmail.com>
2019-03-11 16:29:59 -0700

    made major changes and worked around the PicklingError issue

--- multiproc_wrapper.py
+++ multiproc_wrapper.py
@@ -3,65 +3,64 @@
 # a multiprocessing wrapper for nh3_testcubes.py
 
 #=======================================================================================================================
+import os
 import numpy as np
 import pyspeckit.spectrum.models.ammonia_constants as nh3con
 from pyspeckit.spectrum.units import SpectroscopicAxis as spaxis
 from multiprocessing import Pool, cpu_count
 import tqdm
+import itertools
+from itertools import repeat as rp
 
 import nh3_testcubes as testcubes
-
-class TestSet:
-    # store parameters generated for the test set
-    def __init__(self, nCubes, nComps, nBorder, xarr, paras, grads, noise_rms, linename, output_dir):
-        self.nCubes = nCubes
-        self.nComps = nComps
-        self.nBorder = nBorder
-        self.xarr = xarr
-        self.paras = paras
-        self.grads = grads
-        self.noise_rms = noise_rms
-        self.linename = linename
-        self.output_dir = output_dir
-
-
-def make_n_write(i):
-    # the wrapper function that will be multi-processed
-    testcubes.make_and_write(tset.nCubes, tset.nComps[i], i, tset.nBorder, tset.xarr,
-                             tset.paras['Temp'][i], tset.paras['Width'][i],
-                             tset.paras['Voff'][i], tset.paras['logN'][i],
-                             tset.grads['gradX'][i], tset.grads['gradY'][i],
-                             tset.noise_rms, tset.linename, tset.output_dir)
-
 
 def generate_cubes(nCubes=100, nBorder=1, noise_rms=0.1, output_dir='random_cubes', random_seed=None,
                    linenames=['oneone', 'twotwo'], n_cpu=None):
 
-    global tset
-
-    xarrList = []
-    for linename in linenames:
-        # generate spectral axis for each ammonia lines
-        xarr = spaxis((np.linspace(-500, 499, 1000) * 5.72e-6
-                       + nh3con.freq_dict[linename] / 1e9),
-                      unit='GHz',
-                      refX=nh3con.freq_dict[linename] / 1e9,
-                      velocity_convention='radio', refX_unit='GHz')
-        xarrList.append(xarr)
-
-    # generate random parameters for nCubes
-    nComps, Temp, Width, Voff, logN = testcubes.generate_parameters(nCubes, random_seed)
-    gradX, gradY = testcubes.generate_gradients(nCubes, random_seed)
-    paras = {'Temp':Temp, 'Width':Width, 'Voff':Voff, 'logN':logN}
-    grads = {'gradX':gradX, 'gradY':gradY}
+    if not os.path.isdir(output_dir):
+        #  Make the directory first to avoid multiple processors attempting to make the directory at the same time
+        os.mkdir(output_dir)
 
     if n_cpu is None:
         n_cpu = cpu_count() - 1
         print "number of cpu used: {}".format(n_cpu)
 
-    for xarr, linename in zip(xarrList, linenames):
-        tset = TestSet(nCubes, nComps, nBorder, xarr, paras, grads, noise_rms, linename, output_dir)
+    pool = Pool(n_cpu)
+
+
+    i = np.arange(nCubes)
+    nComp, T, W, V, N = testcubes.generate_parameters(nCubes, random_seed)
+    grdX, grdY = testcubes.generate_gradients(nCubes, random_seed)
+
+
+    for linename in linenames:
         print('----------- generating {0} lines ------------'.format(linename))
-        pool = Pool(n_cpu)
-        for i in tqdm.tqdm(pool.imap(make_n_write, range(nCubes)), total=nCubes, mininterval=0.01):
+        for j in tqdm.tqdm(pool.imap(f_star, itertools.izip(rp(nCubes), nComp, i, rp(nBorder), T, W, V, N, grdX, grdY,
+                                        rp(noise_rms), rp(linename), rp(output_dir))), total=nCubes, mininterval=0.01):
             pass
+
+def f(nCubes, nComp, i, nBorder, T, W, V, N, grdX, grdY, noise_rms, linename, output_dir):
+    # wrapper function to generate and write the test cubes
+
+    # generating SpectroscopicAxis objects here may be a little inefficient, but it's a work around to the pickling
+    # error (i.e., PicklingError: Can't pickle <type 'function'>: attribute lookup __builtin__.function failed issue)
+    xarr = generate_xarr(linename)
+    testcubes.make_and_write(nCubes, nComp, i, nBorder, xarr, T, W, V, N, grdX, grdY, noise_rms, linename, output_dir)
+
+
+def f_star(paras):
+    """Convert `f([a,b,...])` to `f(a,b,...)` call."""
+    return f(*paras)
+
+
+def generate_xarr(linename):
+    # generate SpectroscopicAxis objects
+    xarr = spaxis((np.linspace(-500, 499, 1000) * 5.72e-6
+                   + nh3con.freq_dict[linename] / 1e9),
+                  unit='GHz',
+                  refX=nh3con.freq_dict[linename] / 1e9,
+                  velocity_convention='radio', refX_unit='GHz')
+    return xarr
+
+
+
